96c2991860d6a266f64c8057484aa19d3be13d48
JosDan-gt <jaldanaa3@miumg.edu.gt>
2024-09-22 10:53:08 -0600

    arreglo5

--- front-ares/src/components/Ventas/DetalleVentaForm.jsx
+++ front-ares/src/components/Ventas/DetalleVentaForm.jsx
@@ -93,6 +93,11 @@ const DetalleVentaForm = ({ venta, isEditing, onCancel, onSubmit }) => {
     const errors = {};
     if (!formData.clienteId) errors.clienteId = 'Por favor, seleccione un cliente.';
     if (!formData.fechaVenta) errors.fechaVenta = 'Por favor, seleccione una fecha de venta.';
+    if (!formData.productoId) errors.fechaVenta = 'Por favor, seleccione al menos un producto.';
+    if (!formData.tipoEmpaque) errors.fechaVenta = 'Por favor, seleccione una opcion';
+    if (!formData.tamanoHuevo) errors.fechaVenta = 'Por favor, seleccione un tamaño ';
+    if (!formData.cantidadVendida || formData.cantidadVendida <= 0) errors.fechaVenta = 'Por favor, ingrese una cantidad';
+    if (!formData.precioUnitario || formData.precioUnitario <=0) errors.fechaVenta = 'Por favor, ingrese una cantidad';
     if (formData.detallesVenta.length === 0) errors.detallesVenta = 'Debe agregar al menos un detalle de venta.';
     setFieldErrors(errors);
     return Object.keys(errors).length === 0;
