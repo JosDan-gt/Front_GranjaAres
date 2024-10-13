868f494ac763b2118776fc65f346b798f3565f7d
JosDan-gt <jaldanaa3@miumg.edu.gt>
2024-10-13 14:05:48 -0600

    configurar la PWA2

--- front-ares/src/components/Login/Login.jsx
+++ front-ares/src/components/Login/Login.jsx
@@ -24,8 +24,6 @@ function Login() {
         password,
       });
 
-      console.log('Respuesta completa del backend:', response.data);
-
       const token = response.data.accessToken;
 
       if (token) {
